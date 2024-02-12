85a8199d89973e6f802e2b8d6f7c8211238ce25b
Sangmin-Jeong <serim745@gmail.com>
2024-02-12 01:20:54 -0500

    [ADD] GetPlayerLevel from PlayerState & Combat Interface

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -20,11 +20,14 @@ class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInte
 public:
 	AAuraPlayerState();
 
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 	UAttributeSet* GetAttributeSet() const {return AttributeSet;}
 
-	
+	// FORCEINLINE for small optimization
+	FORCEINLINE int32 GetPlayerLevel() const {return Level;}
 
 protected:
 	UPROPERTY(VisibleAnywhere)
@@ -32,5 +35,13 @@ protected:
 
 	UPROPERTY()
 	TObjectPtr<UAttributeSet> AttributeSet;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_Level)
+	int32 Level = 1;
+
+	UFUNCTION()
+	void OnRep_Level(int32 OldLevel);
 	
 };
