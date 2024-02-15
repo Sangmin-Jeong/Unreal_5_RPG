84fe23b0744bc5e9dc97cbc24c431980da508eaa
Sangmin-Jeong <serim745@gmail.com>
2024-02-14 19:00:33 -0500

    [ADD] UGameplayModMagnitudeCalculation for Applying calculated magnitude in C++

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
@@ -0,0 +1,40 @@
+// Copyright_Sangmin_Jeong
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxHealth::UMMC_MaxHealth()
+{
+	VigorDef.AttributeToCapture = UAuraAttributeSet::GetVigorAttribute();
+	VigorDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	/*
+	 * Snapshot : true = Apply effect right away / false = Apply effect exact time when effect applies
+	 * For now, I don't need Snapshot because the time doesn't matter
+	 * */
+	VigorDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(VigorDef);
+}
+
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Gather tags from Source and Target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+
+	float Vigor = 0.f;
+	GetCapturedAttributeMagnitude(VigorDef, Spec, EvaluateParameters, Vigor);
+	Vigor = FMath::Max(Vigor, 0.f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return 80.0f + 2.5f * Vigor + 10.0f * PlayerLevel;
+}
---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
@@ -0,0 +1,40 @@
+// Copyright_Sangmin_Jeong
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntelligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntelligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	/*
+	 * Snapshot : true = Apply effect right away / false = Apply effect exact time when effect applies
+	 * For now, I don't need Snapshot because the time doesn't matter
+	 * */
+	IntelligenceDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(IntelligenceDef);
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Gather tags from Source and Target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+
+	float Int = 0.f;
+	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, EvaluateParameters, Int);
+	Int = FMath::Max(Int, 0.f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return 50.0f + 2.5f * Int + 15.0f * PlayerLevel;
+}
