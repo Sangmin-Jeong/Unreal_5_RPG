f918b825aceb5ab029df2b75ada48e27f8e1e04d
Sangmin-Jeong <serim745@gmail.com>
2024-02-19 23:29:20 -0500

    [ADD] AttributeInfo with Data Asset & Native GameplayTags

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -16,7 +16,21 @@ public:
 	static const FAuraGameplayTags& Get() {return GameplayTags;}
 	static void InitializeNativeGameplayTags();
 
+	FGameplayTag Attribute_Primary_Strength;
+	FGameplayTag Attribute_Primary_Intelligence;
+	FGameplayTag Attribute_Primary_Resilience;
+	FGameplayTag Attribute_Primary_Vigor;
+	
+	FGameplayTag Attributes_Secondary_MaxHealth;
+	FGameplayTag Attributes_Secondary_MaxMana;
 	FGameplayTag Attributes_Secondary_Armor;
+	FGameplayTag Attributes_Secondary_ArmorPenetration;
+	FGameplayTag Attributes_Secondary_BlockChance;
+	FGameplayTag Attributes_Secondary_CriticalHitChance;
+	FGameplayTag Attributes_Secondary_CriticalHitDamage;
+	FGameplayTag Attributes_Secondary_CriticalHitResistance;
+	FGameplayTag Attributes_Secondary_HealthRegeneration;
+	FGameplayTag Attributes_Secondary_ManaRegeneration;
 	
 protected:
 
